fac4c09d11d637127271815d4bf5205c44152e6f
lisaiundralandi <alicjadudzinska5@gmail.com>
2021-05-31 23:20:29 +0200

    dodanie walidacji formularza kontaktowego

---
+++ kontakt.js
@@ -0,0 +1,29 @@
+function sprawdzImie() {
+    const imie = kontakt.imie.value;
+
+    if (!isNaN(imie) || imie.length < 2) {
+        alert("Niepoprawne imię");
+        return;
+    }
+
+    if (!/^[A-Za-zĄąĘęÓóŚśŁłĆćŻżŹźŃń]+$/.test(imie)) {
+        alert("Imię zawiera niepoprawne znaki");
+        return;
+    }
+
+    kontakt.imie.value = imie.substring(0, 1).toUpperCase() + imie.substring(1).toLowerCase();
+}
+
+function sprawdzMail() {
+    if (!isNaN(kontakt.email.value) || !kontakt.email.value.includes('@')) {
+        alert("Niepoprawny adres email");
+    }
+}
+
+function sprawdzTresc() {
+    const tresc = kontakt.tresc.value;
+
+    if (tresc.length < 10) {
+        alert("Treść musi mieć minimum 10 znaków");
+    }
+}

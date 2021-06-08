ad1c0517fdb8d7c033d89b93b0ee0bd6e9381f95
lisaiundralandi <alicjadudzinska5@gmail.com>
2021-06-08 19:50:20 +0200

    poprawienie walidacji formularza

--- skrypty/kontakt.js
+++ skrypty/kontakt.js
@@ -3,21 +3,25 @@ function sprawdzImie() {
 
     if (!isNaN(imie) || imie.length < 2) {
         alert("Niepoprawne imię");
-        return;
+        return false;
     }
 
     if (!/^[A-Za-zĄąĘęÓóŚśŁłĆćŻżŹźŃń]+$/.test(imie)) {
         alert("Imię zawiera niepoprawne znaki");
-        return;
+        return false;
     }
 
     kontakt.imie.value = imie.substring(0, 1).toUpperCase() + imie.substring(1).toLowerCase();
+
+    return true;
 }
 
 function sprawdzMail() {
     if (!isNaN(kontakt.email.value) || !kontakt.email.value.includes('@')) {
         alert("Niepoprawny adres email");
+        return false;
     }
+    return true;
 }
 
 function sprawdzTresc() {
@@ -27,6 +31,9 @@ function sprawdzTresc() {
         alert("Treść musi mieć minimum 10 znaków");
         return false;
     }
-
     return true;
+}
+
+function sprawdzFormularz(){
+    return sprawdzImie() && sprawdzMail() && sprawdzTresc();
 }
